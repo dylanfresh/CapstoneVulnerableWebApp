7d530bbc4fe9de8c1d6334337cfbab0e8028ebb1
dylanfresh <dylanjfreshman@gmail.com>
2025-11-06 14:40:04 -0800

    Add Secure Login Endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pymysql
-from flask import Flask, request
+from flask import Flask, request, session
 
 """
 app.py: WSRP web application entry point
@@ -48,6 +48,35 @@ def login():
         session['logged_user'] = username
 
     db.close()
+
+
+@app.route('/secure/login')
+def secure_login():
+    """
+    Login endpoint prototype SECURE:
+
+    This implementation is based on "SQL Injection in Python" from SecureFlag
+    (https://knowledge-base.secureflag.com/vulnerabilities/sql_injection/sql_injection_python.html)
+    and "Preventing SQL Injection Attacks With Python" from RealPython
+
+    Notes: will need to be refactored to accommodate potential changes in
+    database (PostGreSQL, MySQL, etc.) and developments in SQL injection
+    testing and mitigation
+    """
+    username = request.values.get('username')
+    password = request.values.get('password')
+
+    db = pymysql.connect("localhost")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = %s AND password = \
+                    %s" % (username, password))
+
+    record = cursor.fetchone()
+    if record:
+        session['logged_user'] = username
+
+    db.close()
+
 
 
 if __name__ == '__main__':
