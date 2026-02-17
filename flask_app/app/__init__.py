b05a8174e2174d7799546fefc2be6ac2ced77fb2
Alyce Harlan <alycelharlan@hotmail.com>
2026-02-17 10:22:21 -0800

    integrated a database setup with the existing flask infrastructure

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
-from app.config import Config
+from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(): #config_class=Config
     '''Function to use as a factory and house all the blueprints'''
     app = Flask(__name__)
-    # app.config.from_object(config_class)
+    app.config.from_object(Config)
+
+    # database set up
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # Initialize Flask extensions here
 
